87e8ab835a4daec43e217398666a406aaf93abe7
Sergei Ermilov <sergei.m.ermilov@gmail.com>
2026-04-21 00:46:28 +0300

    moved to Astro and removed old files

---
+++ src/content.config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders'; // Добавили эту строку
+
+const portfolio = defineCollection({
+  // Теперь используем loader вместо типа 'content'
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/portfolio" }),
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    image: image(), 
+    category: z.string().optional(),
+  }),
+});
+
+export const collections = { portfolio };
